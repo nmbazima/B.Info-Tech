d17ad34527805408afab822fc91c3a7330e841df
Newton Mbazima <31067101+nmbazima@users.noreply.github.com>
2020-11-10 11:46:16 +0200

    Updates on Programming in C

---
+++ COM213/Examples/SizeOf(type).c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    /*Example to get the size of int type on any machine*/
+    printf("Storage size for int : %d \n", sizeof(int));
+
+    return 0;
+}
