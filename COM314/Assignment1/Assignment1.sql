40ad58b524339e6489699b7f707c55b0930d3147
Newton Mbazima <31067101+nmbazima@users.noreply.github.com>
2022-02-27 18:55:54 +0200

    Added table

---
+++ COM314/Assignment1/Assignment1.sql
@@ -0,0 +1,18 @@
+-- Question 1 of assigment, create DB named CUZ2022. 
+-- Tables required STUDENTS, CLASS, LECTURER & ENROLMENT
+-- Students (Student No., Name, Address, Phone)
+-- Class (Course Level, Subject, Class No. Lecturer No., Room No., Time, Fee)
+-- Lecturer (Lecturer No., Lecturer Name, Address, Phone)
+-- Enrolment (Student No, Course Level, Subject, Class No.)
+
+-- CREATE DATABASE CUZ2022
+-- GO;
+
+CREATE TABLE Students
+(
+    StudentNo INT NOT NULL
+    ,StudentName INT
+    ,StudentAddress INT
+    ,StudentPhone INT
+);
+
